bae1791c7a49fd4170f6b0e5c6cd5487c512b967
Rohmat setiadi <111052716+rs-adii@users.noreply.github.com>
2025-04-06 20:54:58 +0700

    add code in main.js

--- main.js
+++ main.js
@@ -1,31 +1,53 @@
-document.addEventListener('DOMContentLoaded', loadTask);
+const jsxDevRuntime = require("react/jsx-dev-runtime");
 
-function addTask () {
-    let input = document.getElementById('taskInput');
-    let taskText = input.value.trim();
-    if(!taskText) {
-        return;
-    }
-    let li = document.createElement('li');
-    li.innerHTML = `<span>${taskText}</span><button onclick="removeTask(this)>X</button>`;
-    document.getElementById('taskList').appendChild(li);
+document.addEventListener("DOMContentLoaded", loadTask);
 
-    input.value = '';
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value.trim();
+  if (!taskText) {
+    return;
+  }
+  
+  let li = document.createElement("li");
+  li.innerHTML = `<span>${taskText}</span><button onclick="removeTask(this)>X</button>`;
+  document.getElementById("taskList").appendChild(li);
+  saveTask(taskText);
+  input.value = "";
 }
 
 function removeTask(button) {
-    button.parentElement.remove();
+  button.parentElement.remove();
 }
 
 function toggleComplete(task) {
-    task.classList.toggle('completed');
+  task.classList.toggle("completed");
 }
 
-function saveTask() {
+function saveTask(taskText) {
+  let tasks = loadTask();
+  tasks.push(taskText);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  console.log("Tasks", tasks);
+}
 
+function getTaskFromStorage() {
+  try {
+    let tasks = JSON.parse(localStorage.getItem("tasks") || []);
+    console.log("loadTask", tasks);
+  } catch (error) {
+    tasks = [];
+    console.log(error);
+  }
+  // console.log('loadTask', tasks);
+  return tasks;
 }
 
 function loadTask() {
-let tasks = localStorage.getItem('tasks');
-console.log('loadTask', tasks);
+    let tasks = getTaskFromStorage();
+    tasks.forEach((task) => {
+        let li = document.createElement("li");
+        li.innerHTML = `<span>${task}</span><button onclick="removeTask(this)>X</button>`;
+        document.getElementById("taskList").appendChild(li);  
+    });
 }
